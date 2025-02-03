4a5918f692af2b9fd002571cc68233eae40dee66
Tederr <jp3351207@gmail.com>
2025-02-03 13:19:00 -0300

    card about

--- src/scripts/pokemonDetails.js
+++ src/scripts/pokemonDetails.js
@@ -1,3 +1,6 @@
+const types = document.getElementById('pokemon-types');
+const backgraudColor = document.querySelector('.content-details');
+
 async function fetchPokemonData(pokemonId) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
     const data = await response.json();
@@ -5,21 +8,42 @@ async function fetchPokemonData(pokemonId) {
 }
 
 // Função para exibir os detalhes do Pokémon
-const backgraudColor = document.querySelector('.content-details');
+
+function formatId(pokemonId){
+    if(pokemonId < 10){
+        return `#00${pokemonId}`
+    }else if(pokemonId < 100){
+        return `#0${pokemonId}`
+    }else{
+        return `#${pokemonId}`
+    }
+}
+
+function convertMeters(Altura) {
+    const height = Altura / 10;
+    return `${height}m`;
+}
+
+function convertKg(peso) {
+    const weight = peso / 10;
+    return `${weight}kg`;
+}
+
 function convertType(pokemon) {
     backgraudColor.classList.add(pokemon.types[0].type.name);
     return `${pokemon.types.map((type) => `<li class="type-details ${type.type.name}">${type.type.name}</li>`).join('')}`;
 }
 
-const types = document.getElementById('pokemon-types');
 function displayPokemonDetails(pokemon) {
     document.getElementById('pokemon-name').textContent = pokemon.name;
-    document.getElementById('pokemon-id').textContent = `#${pokemon.id}`;
+    document.getElementById('pokemon-id').textContent = `${formatId(pokemon.id)}`;
     types.innerHTML = convertType(pokemon);
-
     document.getElementById('pokemon-image').src = pokemon.sprites.other['official-artwork'].front_default;
-    document.getElementById('pokemon-height').textContent = `Altura: ${pokemon.height / 10}m`;
-    // document.getElementById('pokemon-weight').textContent = `Peso: ${pokemon.weight / 10}kg`;
+
+    document.getElementById('pokemon-height').textContent = `${convertMeters(pokemon.height)}`;
+    document.getElementById('pokemon-weight').textContent = `${convertKg(pokemon.weight)}`;
+    document.getElementById('pokemon-abilities').textContent = `${pokemon.abilities.map((ability) => ability.ability.name).join(', ')}`;
+
 }
 
 // Função principal para carregar os detalhes do Pokémon
